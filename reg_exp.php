f9fa92982d7990735510d7fc4299ea80e5587c82
maxuan <1264565530@qq.com>
2014-09-07 09:48:41 +0800

    一些正则表达式

---
+++ reg_exp.php
@@ -0,0 +1,4 @@
+<?php
+$str = '[url]1.gif[/url][url]2.gif[/url][url]3.gif[/url]';
+$s = preg_replace("#\[url\](?<WORD>\d\.gif)\[\/url\]#","<img src=http://image.ai.com/upload/$1>",$str);
+var_dump($s);
