181adff50e5bc1bbecea53d65ceb8bc9a00da86f
maxuan <1264565530@qq.com>
2014-09-07 10:01:06 +0800

    添加注释

--- reg_exp.php
+++ reg_exp.php
@@ -1,4 +1,6 @@
 <?php
+
+//反向引用
 $str = '[url]1.gif[/url][url]2.gif[/url][url]3.gif[/url]';
 $s = preg_replace("#\[url\](?<WORD>\d\.gif)\[\/url\]#","<img src=http://image.ai.com/upload/$1>",$str);
 var_dump($s);
